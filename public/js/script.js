d6d0ac07172162165337463a8dcf4a8a11db9bc0
joaomarques <joaofmarques.dev@gmail.com>
2023-10-22 19:55:01 -0300

    adequação de estrutura para segurança do código

---
+++ public/js/script.js
@@ -0,0 +1,6 @@
+let button = document.querySelector('button[name = button-send]');
+let inputContent = document.querySelector('input[name = content]');
+
+button.addEventListener('click',()=>{setTimeout(() => {
+  inputContent.value = " ";
+}, 1000)});
